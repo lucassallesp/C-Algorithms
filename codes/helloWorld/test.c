dd40db6e7eb3a8b6beb46116848b9917bc08e693
Lucas P. Salles <94552900+lucassallesp@users.noreply.github.com>
2022-07-23 15:09:59 -0300

    Add files via upload

---
+++ codes/helloWorld/test.c
@@ -0,0 +1,57 @@
+//==============================================================================
+//
+// Title:		test.c
+// Purpose:		A short description of the implementation.
+//
+// Created on:	16/11/2020 at 11:37:38 by Lucas Salles.
+// Copyright:	. All Rights Reserved.
+//
+//==============================================================================
+
+//==============================================================================
+// Include files
+
+//#include "test.h"
+
+//==============================================================================
+// Constants
+
+//==============================================================================
+// Types
+
+//==============================================================================
+// Static global variables
+
+//==============================================================================
+// Static functions
+
+//==============================================================================
+// Global variables
+
+//==============================================================================
+// Global functions
+
+/// HIFN  What does your function do?
+/// HIPAR x/What inputs does your function expect?
+/// HIRET What does your function return?
+int Define_Your_Functions_Here (int x)
+{
+	return x;
+}
+		 
+
+#include <ansi_c.h>
+#include <cvirte.h>
+
+
+#include <cvirte.h>
+
+int main ()
+{
+	printf("Hello World");
+	getchar();
+		
+return 0;
+	
+}
+
